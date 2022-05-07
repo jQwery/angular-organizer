bc9cce4218da51e7f1fce40358b35697bde0b8d3
jQwery <75556402+jQwery@users.noreply.github.com>
2022-05-07 13:39:58 +0300

    Создан элемент календаря, создана база данных FireBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SelectorComponent } from './selector/selector.component';
 import { OrganizerComponent } from './organizer/organizer.component';
 import { MomentPipe } from './shared/moment.pipe';
 import { DateService } from './shared/date.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,10 +15,13 @@ import { DateService } from './shared/date.service';
     CalendarComponent,
     SelectorComponent,
     OrganizerComponent,
-    MomentPipe
+    MomentPipe,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [DateService],
   bootstrap: [AppComponent]
